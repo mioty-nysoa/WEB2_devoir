ea2de29524340d5e0e2a67b4d07ac8481024554b
Mioty Rajosefa <hei.mioty.3@gmail.com>
2026-08-13 20:36:41 +0300

    last commit

--- src/Repository/studentRepository.ts
+++ src/Repository/studentRepository.ts
@@ -5,17 +5,18 @@ const pool = new Pool({
     user: 'postgres',
     host: 'localhost',
     database: 'students',
+    password:'1234',
     port: 5432,
     });
 
 export class studentRepository {
     async getAll(): Promise<Student[]> {
-        const result = await pool.query('SELECT * FROM students');
+        const result = await pool.query('SELECT * FROM student');
         return result.rows;
     }
 
     async getById(id: number): Promise<Student | null> {
-        const result = await pool.query('SELECT * FROM students WHERE id = $1', [id]);
+        const result = await pool.query('SELECT * FROM student WHERE id = $1', [id]);
         if (result.rows.length === 0) {
     return null;
   }
@@ -24,12 +25,12 @@ export class studentRepository {
     }
 
     async create(name: string, age: number): Promise<Student> {
-        const result = await pool.query('INSERT INTO students (name, age) VALUES ($1, $2) RETURNING *', [name, age]);
+        const result = await pool.query('INSERT INTO student (name, age) VALUES ($1, $2) RETURNING *', [name, age]);
         return result.rows[0];
     }
 
     async update(id: number, name: string, age: number): Promise<Student | null> {
-        const result = await pool.query('UPDATE students SET name = $1, age = $2 WHERE id = $3 RETURNING *', [name, age, id]);
+        const result = await pool.query('UPDATE student SET name = $1, age = $2 WHERE id = $3 RETURNING *', [name, age, id]);
         if (result.rows.length === 0) {
             return null;
         }
@@ -37,6 +38,6 @@ export class studentRepository {
     }
 
     async delete(id: number): Promise<void> {
-        await pool.query('DELETE FROM students WHERE id = $1', [id]);
+        await pool.query('DELETE FROM student WHERE id = $1', [id]);
     }
 }
